5bded433699a47139b2f6066210de7edd461a9f1
dgeibi <png.inside@gmail.com>
2017-03-27 14:09:56 +0800

    separate some modules

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 /* eslint-disable no-underscore-dangle */
 const fsp = require('fs-promise')
-const htmlclean = require('htmlclean')
 const path = require('path')
 const chokidar = require('chokidar')
 
 const { readMD, writeMD, isMarkdown, renderMD } = require('./lib/plugins/mdKit')
 const renderer = require('./lib/plugins/renderer')
+const { writeIndex, writeDocs } = require('./lib/plugins/writer')
+const fillInfo = require('./lib/plugins/fillInfo')
 
 const server = require('./lib/utils/server')
 const logger = require('./lib/utils/log')
-const findDocs = require('./lib/utils/findDocs')
+
 const { glob } = require('./lib/utils/promisified')
 const capitalize = require('./lib/utils/capitalize')
 
-const addTOC = require('./lib/views/addTOC')
-const getList = require('./lib/views/getList')
-const getNav = require('./lib/views/getNav')
-
-
-const defaultConfig = {
-  root: '.',
-  docsPath: '_note',
-  title: '',
-  layout: 'default',
-  overwriteIndex: false,
-  indexLayout: 'index',
-  baseurl: '',
-  port: 4000,
-  layoutPath: '_layouts',
-  publicPath: 'docs',
-  excludes: [],
-  typeMap: {},
-}
+const defaultConfig = require('./lib/defaultConfig.json')
 
 class Wikic {
   constructor(cwd) {
-    this.writeDocs = this.writeDocs.bind(this)
-    this.writeIndex = this.writeIndex.bind(this)
+    this.renderer = renderer
+    this.fillInfo = fillInfo.bind(this)
     this.buildStaticFile = this.buildStaticFile.bind(this)
-    this.renderDocs = this.renderDocs.bind(this)
-    this.renderIndex = this.renderIndex.bind(this)
-    this.fillInfo = this.fillInfo.bind(this)
     this.typeMap = this.typeMap.bind(this)
     this.setup(cwd)
   }
@@ -97,6 +77,14 @@ class Wikic {
     this.config = Object.assign({}, defaultConfig)
     const rootConfig = fsp.readJsonSync(path.join(this.cwd, '_config.json'))
     Object.assign(this.config, rootConfig)
+
+    const { root, publicPath, docsPath, layoutPath } = this.config
+    this.root = path.resolve(this.cwd, root)
+    this.publicPath = path.resolve(this.root, publicPath)
+    this.layoutPath = path.resolve(this.root, layoutPath)
+    this.docsPath = path.resolve(this.root, docsPath)
+
+    this.configureRenderer()
     return this
   }
 
@@ -104,8 +92,6 @@ class Wikic {
     this.cwd = path.resolve(cwd)
     this.setConfig()
       .setBaseURL()
-      .setPaths()
-      .configureRenderer()
     return this
   }
 
@@ -154,73 +140,12 @@ class Wikic {
     this.setup().render()
   }
 
-  setPaths() {
-    const { root, publicPath, docsPath, layoutPath } = this.config
-    this.root = path.resolve(this.cwd, root)
-    this.publicPath = path.resolve(this.root, publicPath)
-    this.layoutPath = path.resolve(this.root, layoutPath)
-    this.docsPath = path.resolve(this.root, docsPath)
-    return this
-  }
-
-  fillInfo({ data, config }) {
-    const { types, address, attributes } = config
-    const { title, hide } = attributes
-    if (hide) return { data, config }
-    if (typeof this.docsInfos !== 'object' && !this.docsInfos) this.docsInfos = {}
-    const docInfo = {
-      address,
-      title,
-    }
-    /* eslint-disable no-param-reassign */
-    types.reduce((parent, type, index, arr) => {
-      if (typeof parent[type] !== 'object') {
-        parent[type] = { _docs: [] }
-      }
-      if (index === arr.length - 1) {
-        parent[type]._docs.push(docInfo)
-      }
-      return parent[type]
-    }, this.docsInfos)
-    /* eslint-enable no-param-reassign */
-    return { data, config }
-  }
-
-  renderIndex({ config }) {
-    const { types, indexLayout } = config
-    const collections = (types[0] === '.') ? this.docsInfos : findDocs(this.docsInfos, types)
-    const content = getList(types, collections, this)
-    const navbar = getNav(types, this)
-    const type = types.map(this.typeMap).join(' - ')
-    const newData = renderer.render(`${indexLayout}.njk`, {
-      navbar,
-      content,
-      type,
-    })
-    return newData
-  }
-
-  renderDocs({ data, config }) {
-    const { types, attributes } = config
-    const layout = attributes.layout || config.layout
-    const title = attributes.title
-    const navbar = getNav(types, this)
-    const type = types.map(this.typeMap).join(' - ')
-    const page = { title }
-    const html = renderer.render(`${layout}.njk`, {
-      content: data,
-      navbar,
-      type,
-      page,
-    })
-    return html
-  }
-
-
   buildDocs() {
     this.docsInfos = {}
+    const write = writeDocs.bind(this)
+
     return glob('**/*.md', { cwd: this.docsPath })
-      .then(files => Promise.all(files.map(this.writeDocs)))
+      .then(files => Promise.all(files.map(write)))
   }
 
   async render() {
@@ -232,40 +157,10 @@ class Wikic {
   }
 
   async buildIndex() {
+    const write = writeIndex.bind(this)
     const dirs = await glob('**/', { cwd: this.docsPath })
     dirs.push('./')
-    await Promise.all(dirs.map(this.writeIndex))
-  }
-
-  async writeDocs(file) {
-    const targetRelative = file.replace(/\.md$/, '.html')
-
-    const from = path.join(this.docsPath, file)
-    const to = path.join(this.publicPath, targetRelative)
-
-    const types = path.dirname(file).split('/')
-    const address = path.join('/', targetRelative)
-    const config = Object.assign({}, this.config, { types, address })
-
-    const result = await readMD(from, config)
-    const html = await Promise.resolve(result)
-      .then(this.fillInfo)
-      .then(this.renderDocs)
-      .then(addTOC)
-    await writeMD(to, html)
-  }
-
-  writeIndex(dir) {
-    const dirname = dir.slice(0, dir.length - 1)
-    const types = dirname.split('/')
-    const target = path.join(this.publicPath, dirname, 'index.html')
-    const address = path.join('/', dirname, '/')
-    const config = Object.assign({}, this.config, { types, address })
-    return Promise.resolve({ config })
-      .then(this.renderIndex)
-      .then(htmlclean)
-      .then(html => fsp.outputFile(target, html))
-      .catch(e => logger.error(e))
+    await Promise.all(dirs.map(write))
   }
 
   async buildStaticFile(filePath) {
