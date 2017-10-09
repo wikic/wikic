0409e9639cb60fadecad59dd25d71d429aec311a
dgeibi <png.inside@gmail.com>
2017-10-09 21:29:14 +0800

    Feat: watch handlers

--- __mocks__/chokidar.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events')
-/* eslint-disable no-underscore-dangle */
-const emiter = new EventEmitter()
-const chokidar = jest.genMockFromModule('chokidar')
-chokidar.watch = jest.fn(() => emiter)
-chokidar.__emiter = emiter
-
-module.exports = chokidar
